6a1c32574956599bbbf7f81488c519d474a953c7
Andrés Javier Rebolledo Serrato <andres.jrserrato@gmail.com>
2022-03-23 01:29:24 -0500

    Segunda parte ejercicio 1 terminado de Introducción

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        int resultado = 0;
-        resultado = suma( 25,  70,  65);
-        System.out.println(resultado);
+	    coche miCoche = new coche();
+        miCoche.NumPuertas();
+        miCoche.NumPuertas();
+        miCoche.NumPuertas();
+        System.out.println("El coche tiene: " + miCoche.puertas + " puertas.");
     }
     public static int suma(int a, int b, int c){
         return a + b + c;
     }
+}
+
+class coche {
+    public  int puertas = 0;
+
+    public void  NumPuertas() {
+        this.puertas++;
+    }
+
 }
