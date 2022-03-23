a7cb3832524ceb5c0c0570bdadc5125dc25ad4eb
Andrés Javier Rebolledo Serrato <andres.jrserrato@gmail.com>
2022-03-23 00:10:47 -0500

    Primera parte ejercicio 1 terminado de Introducción

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int resultado = 0;
+        resultado = suma( 25,  70,  65);
+        System.out.println(resultado);
+    }
+    public static int suma(int a, int b, int c){
+        return a + b + c;
+    }
+}
